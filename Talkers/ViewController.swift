dcbfb6d445ddab4cb33843f4a0f74c4898024407
KazakovaNetIOS <kazakova.net@gmail.com>
2020-09-11 00:41:36 +0300

    Initial commit

---
+++ Talkers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Talkers
+//
+//  Created by Natalia Kazakova on 11.09.2020.
+//  Copyright © 2020 Natalia Kazakova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
