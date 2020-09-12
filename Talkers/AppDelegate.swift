44d6065d437dd3a2bc7aa8ad093950577c934f60
KazakovaNetIOS <kazakova.net@gmail.com>
2020-09-13 00:44:38 +0300

    Added app icons, logged appDelegate lifecycle methods

--- Talkers/AppDelegate.swift
+++ Talkers/AppDelegate.swift
@@ -10,30 +10,37 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window : UIWindow?
 
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("Launch process has begun but that state restoration has not yet occurred: \(#function). Initializing the app\n")
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
-    @available(iOS 13.0, *)
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        print("Application moved from initialize to launch: \(#function)\n")
+        return true
     }
-
-    @available(iOS 13.0, *)
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        print("\(#function) called to finish up the transition to the foreground\n")
     }
-
-
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        print("Application moved from launch to inactive: \(#function)\n")
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        print("Application moved from inactive to background state: \(#function)\n")
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        print("Application moved from background state to active after interruption: \(#function)\n")
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        print("\(#function) called when app is about to be purged from memory\n")
+    }
 }
 
