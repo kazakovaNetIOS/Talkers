ed92311ca7f34c90d49ae13a3825dd9572ff7fe0
KazakovaNetIOS <kazakova.net@gmail.com>
2020-09-15 00:30:32 +0300

    Logged ViewController lifecycle methods

--- Talkers/AppDelegate.swift
+++ Talkers/AppDelegate.swift
@@ -14,33 +14,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window : UIWindow?
 
     func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        print("Launch process has begun but that state restoration has not yet occurred: \(#function). Initializing the app\n")
+        print("Launch process has begun but that state restoration has not yet occurred: \(#function). Initializing the app")
         return true
     }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        print("Application moved from initialize to launch: \(#function)\n")
+        print("Application moved from initialize to launch: \(#function)")
         return true
     }
     
     func applicationDidBecomeActive(_ application: UIApplication) {
-        print("\(#function) called to finish up the transition to the foreground\n")
+        print("\(#function) called to finish up the transition to the foreground")
     }
     
     func applicationWillResignActive(_ application: UIApplication) {
-        print("Application moved from launch to inactive: \(#function)\n")
+        print("Application moved from launch to inactive: \(#function)")
     }
     
     func applicationDidEnterBackground(_ application: UIApplication) {
-        print("Application moved from inactive to background state: \(#function)\n")
+        print("Application moved from inactive to background: \(#function)")
     }
     
     func applicationWillEnterForeground(_ application: UIApplication) {
-        print("Application moved from background state to active after interruption: \(#function)\n")
+        print("Application moved from background to active after interruption: \(#function)")
     }
     
     func applicationWillTerminate(_ application: UIApplication) {
-        print("\(#function) called when app is about to be purged from memory\n")
+        print("\(#function) called when app is about to be purged from memory")
     }
 }
 
--- Talkers/ViewController.swift
+++ Talkers/ViewController.swift
@@ -12,9 +12,44 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print("The view of UIViewController was loaded into memory: \(#function)")
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("The view of UIViewController is about to be added to a view hierarchy: \(#function)")
+    }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("The view of UIViewController was added to a view hierarchy: \(#function)")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        print("The view of UIViewController is about to layout its subviews: \(#function)")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        print("The view of UIViewController has just laid out its subviews: \(#function)")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        print("The view of UIViewController is about to be removed from a view hierarchy: \(#function)")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        print("The view of UIViewController was removed from a view hierarchy: \(#function)")
+    }
 }
 
