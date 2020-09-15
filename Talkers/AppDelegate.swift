e96faa96177f2f8dfee9561ec6f7099fcbdd09b4
KazakovaNetIOS <kazakova.net@gmail.com>
2020-09-15 12:12:39 +0300

    Added a condition for displaying logs

--- Talkers/AppDelegate.swift
+++ Talkers/AppDelegate.swift
@@ -12,35 +12,41 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window : UIWindow?
-
+    
     func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        print("Launch process has begun but that state restoration has not yet occurred: \(#function). Initializing the app")
+        AppDelegate.printInLog("Launch process has begun but that state restoration has not yet occurred: \(#function). Initializing the app")
         return true
     }
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        print("Application moved from initialize to launch: \(#function)")
+        AppDelegate.printInLog("Application moved from initialize to launch: \(#function)")
         return true
     }
     
     func applicationDidBecomeActive(_ application: UIApplication) {
-        print("\(#function) called to finish up the transition to the foreground")
+        AppDelegate.printInLog("\(#function) called to finish up the transition to the foreground")
     }
     
     func applicationWillResignActive(_ application: UIApplication) {
-        print("Application moved from launch to inactive: \(#function)")
+        AppDelegate.printInLog("Application moved from launch to inactive: \(#function)")
     }
     
     func applicationDidEnterBackground(_ application: UIApplication) {
-        print("Application moved from inactive to background: \(#function)")
+        AppDelegate.printInLog("Application moved from inactive to background: \(#function)")
     }
     
     func applicationWillEnterForeground(_ application: UIApplication) {
-        print("Application moved from background to active after interruption: \(#function)")
+        AppDelegate.printInLog("Application moved from background to active after interruption: \(#function)")
     }
     
     func applicationWillTerminate(_ application: UIApplication) {
-        print("\(#function) called when app is about to be purged from memory")
+        AppDelegate.printInLog("\(#function) called when app is about to be purged from memory")
+    }
+    
+    public static func printInLog(_ text: String) {
+        #if DEBUG
+            print(text)
+        #endif
     }
 }
 
