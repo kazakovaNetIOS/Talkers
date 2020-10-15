b43015da3fedfe837f4002bb0cfd82a84731a0cc
KazakovaNetIOS <kazakova.net@yandex.ru>
2020-10-16 00:54:04 +0300

    Minor fixes

--- Talkers/Storage/FileStorage.swift
+++ Talkers/Storage/FileStorage.swift
@@ -18,18 +18,14 @@ class FileStorage {
   private init() {}
 
   func saveToFile(userProfile: UserProfile) throws {
-    guard let path = UserProfile.ArchiveURL else {
-      throw FileStorageError.runtimeError
-    }
+    guard let path = UserProfile.ArchiveURL else { throw FileStorageError.runtimeError }
 
     let data = try NSKeyedArchiver.archivedData(withRootObject: userProfile, requiringSecureCoding: false)
     try data.write(to: path)
   }
 
   func loadFromFile() throws -> UserProfile? {
-    guard let path = UserProfile.ArchiveURL else {
-      throw FileStorageError.runtimeError
-    }
+    guard let path = UserProfile.ArchiveURL else { throw FileStorageError.runtimeError }
 
     let data = try Data(contentsOf: path)
     return try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? UserProfile
